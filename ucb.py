3de3535aa7d3981af9ad6155978e1077d7d2ece7
Lucahadife <144637618+Lucahadife@users.noreply.github.com>
2024-08-20 17:05:04 +0200

    Update ucb.py

--- ucb.py
+++ ucb.py
@@ -1,4 +1,3 @@
-"""The UCB module contains functions specific to 61A projects at UC Berkeley."""
 
 import code
 import functools
@@ -8,16 +7,7 @@ import sys
 import re
 
 def main(fn):
-    """Call fn with command line arguments.  Used as a decorator.
-
-    The main decorator marks the function that starts a program. For example,
-
-    @main
-    def my_run_function():
-        # function body
-
-    Use this instead of the typical __name__ == "__main__" predicate.
-    """
+    
     if inspect.stack()[1][0].f_locals['__name__'] == '__main__':
         args = sys.argv[1:] # Discard the script name from command line
         fn(*args) # Call the main function
@@ -26,11 +16,7 @@ def main(fn):
 _PREFIX = ''
 def trace(fn):
     """A decorator that prints a function's name, its arguments, and its return
-    values each time the function is called. For example,
-
-    @trace
-    def compute_something(x, y):
-        # function body
+    values each time the function is called.
     """
     @functools.wraps(fn)
     def wrapped(*args, **kwds):
@@ -46,32 +32,25 @@ def trace(fn):
             log(fn.__name__ + ' exited via exception')
             _PREFIX = _PREFIX[:-4]
             raise
-        # Here, print out the return value.
         log('{0}({1}) -> {2}'.format(fn.__name__, ', '.join(reprs), result))
         return result
     return wrapped
 
 
 def log(message):
-    """Print an indented message (used with trace)."""
+    """Prints an indented message."""
     message = str(message)
     print(_PREFIX + re.sub('\n', '\n' + _PREFIX, message))
 
 
 def log_current_line():
-    """Print information about the current line of code."""
+    """Prints information about the current line of code."""
     frame = inspect.stack()[1]
     log('Current line: File "{f[1]}", line {f[2]}, in {f[3]}'.format(f=frame))
 
 
 def interact(msg=None):
-    """Start an interactive interpreter session in the current environment.
-
-    On Unix:
-      <Control>-D exits the interactive session and returns to normal execution.
-    In Windows:
-      <Control>-Z <Enter> exits the interactive session and returns to normal
-      execution.
+    """Starts an interactive interpreter session in the current environment.
     """
     # evaluate commands in current namespace
     frame = inspect.currentframe().f_back
